345723968f5f2fd5048888159f7abc86f9496dbf
Simon Baechler <sb@feinheit.ch>
2011-09-28 21:17:50 +0200

    inital commit

--- README.rst
+++ README.rst
@@ -6,6 +6,7 @@ Requirements
 ------------
 To use this content type, you need the Gdata Python Client Library.
 Download it here: http://code.google.com/p/gdata-python-client/downloads/list
+
 Make a symlink to src/gdata and src/atom
 
 
@@ -14,3 +15,11 @@ Usage
 Check out http://googlesystem.blogspot.com/2008/01/youtube-feeds.html for
 how to get the URL of the feeds.
 
+Add your Youtube developer key to the settings file::
+
+    YOUTUBE_DEV_KEY = "XXXX...........XXXX"
+    YOUTUBE_CLIENT_ID = "any_string"
+    
+
+Youtube feeds are paginated after 25 objects. 
+Since the content type doesn't support pagination yet you are basically limited to 25 videos per feed.
--- contents.py
+++ contents.py
@@ -2,12 +2,10 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.template.loader import render_to_string
-import gdata.youtube
-import gdata.youtube.service
-import gdata.media
-import gdata.geo
+from django import forms
+import gdata
+from gdata.youtube.service import RequestError, YouTubeService
 from django.template.context import RequestContext
-from gdata.service import RequestError
 from django.conf import settings
 from datetime import date
 import random
@@ -19,12 +17,24 @@ Make a symlink to src/gdata and src/atom
 
 Check out http://googlesystem.blogspot.com/2008/01/youtube-feeds.html for
 how to get the URL of the feeds.
+
+Make sure you set the permission on the gdata and atom folder for your webserver.
 """
 
+DIMENSION_CHOICES = (
+    ('504x324', _('Facebook Tab Video')),
+)
+
+YOUTUBE_FORM_MEDIA_DICT = {'youtube_choices': 
+                             {'css':{'all':('content/youtube/choices.css',)},
+                               'js':('content/youtube/jquery.infinitecarousel2.min.js', 
+                                     'content/youtube/choices.js')},
+}
 
 
 YOUTUBE_CHOICES = (
     ('feed', _('Playlist')),
+    ('choices', _('Choices')),
     ('first', _('First video')),
     ('daily', _('Daily video')),
     ('random', _('Random video')),
@@ -33,17 +43,18 @@ YOUTUBE_CHOICES = (
 class YoutubeVideoFeedContent(models.Model):
     
     feed = models.URLField(_('Playlist'), max_length=100,
-                help_text='z.B. http://gdata.youtube.com/feeds/api/users/garyferro/favorites?orderby=updated<br />http://gdata.youtube.com/feeds/api/playlists/50653251EDB4E764')
+                help_text='i.e. http://gdata.youtube.com/feeds/api/users/garyferro/favorites?orderby=updated<br />http://gdata.youtube.com/feeds/api/playlists/50653251EDB4E764')
     
     @classmethod
-    def initialize_type(cls, TYPE_CHOICES=YOUTUBE_CHOICES, DIMENSION_CHOICES=None,
-                        MOVIE_ATTRS=None):
+    def initialize_type(cls, TYPE_CHOICES=YOUTUBE_CHOICES, DIMENSION_CHOICES=DIMENSION_CHOICES,
+                        MOVIE_ATTRS=None, FORM_MEDIA_DICT=YOUTUBE_FORM_MEDIA_DICT):
         cls.add_to_class('type', models.CharField(_('Type'), max_length=10, 
                                 choices=TYPE_CHOICES))
         if DIMENSION_CHOICES is not None:
             cls.add_to_class('dimension', models.CharField(_('dimension'),
                 max_length=12, blank=True, null=True, choices=DIMENSION_CHOICES,
                 default=DIMENSION_CHOICES[0][0]))
+        cls.FORM_MEDIA_DICT = FORM_MEDIA_DICT
     
     class Meta:
         verbose_name = _('Video feed')
@@ -53,8 +64,16 @@ class YoutubeVideoFeedContent(models.Model):
     def __unicode__(self):
         return unicode(self.feed)
     
+    @property
+    def media(self):
+        media = forms.Media()
+        if self.FORM_MEDIA_DICT.get('youtube_%s' % self.type, None):
+            media.add_css(self.FORM_MEDIA_DICT['youtube_%s' % self.type].get('css', {}))
+            media.add_js(self.FORM_MEDIA_DICT['youtube_%s' % self.type].get('js', ''))    
+        return media
+    
     def service(self):
-        yt_service = gdata.youtube.service.YouTubeService()
+        yt_service = YouTubeService()
         yt_service.ssl = False
         yt_service.developer_key = getattr(settings, 'YOUTUBE_DEV_KEY', None)
         yt_service.client_id = getattr(settings, 'YOUTUBE_CLIENT_ID', None)
@@ -70,7 +89,7 @@ class YoutubeVideoFeedContent(models.Model):
             return HttpResponse(ugettext(u'Es gab ein Fehler beim Verbinden mit Youtube. Bitte versuchen Sie es nochmals.'))
         entries = feed.entry
         
-        context = {'entries': entries, 'first': entries[0] }
+        context = {'entries': entries, 'first': entries[0], 'movie': entries[0] }
         if 'dimension' in dir(self):
             context['dimensions'] = self.dimension.split('x')
         
@@ -83,6 +102,9 @@ class YoutubeVideoFeedContent(models.Model):
         elif self.type == 'random':
             if len(entries) > 0:
                 context ['movie'] = random.choice(entries)
+                
+        if settings.DEBUG:
+            context['self'] = self
         
         return render_to_string(['content/youtube/%s.html' % self.type, 
                                  'content/youtube/default.html'], 
